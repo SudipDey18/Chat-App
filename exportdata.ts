06ccf6b1aaf227e901fa568fe445b9df4cf363de
SudipDey18 <sdey7584@gmail.com>
2026-07-24 13:19:04 +0530

    nitification sound updated

--- exportdata.ts
+++ exportdata.ts
@@ -1,3 +1,4 @@
-// export const API_URl = "https://chatapp.sudip.fun"
+export const API_URl =
+  "https://sudip-chatapp.boxd.sh";
 // export const API_URl = "http://192.168.1.16:3000"
-export const API_URl = "http://10.80.93.187:3000";
+// export const API_URl = "http://10.80.93.187:3000";
